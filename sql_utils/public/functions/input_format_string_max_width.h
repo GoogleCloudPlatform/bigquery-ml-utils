8b4862e99d4501cabf7935d0e6a05e9825963948
BigQuery ML Team <bqml-team@google.com>
2023-07-24 07:11:04 -0700

    No public description
    PiperOrigin-RevId: 550548124

---
+++ sql_utils/public/functions/input_format_string_max_width.h
@@ -0,0 +1,26 @@
+//
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef THIRD_PARTY_PY_BIGQUERY_ML_UTILS_SQL_UTILS_PUBLIC_FUNCTIONS_INPUT_FORMAT_STRING_MAX_WIDTH_H_
+#define THIRD_PARTY_PY_BIGQUERY_ML_UTILS_SQL_UTILS_PUBLIC_FUNCTIONS_INPUT_FORMAT_STRING_MAX_WIDTH_H_
+
+#include <cstdint>
+
+#include "absl/flags/declare.h"
+
+ABSL_DECLARE_FLAG(int32_t, sql_cast_format_string_max_width);
+
+#endif  // THIRD_PARTY_PY_BIGQUERY_ML_UTILS_SQL_UTILS_PUBLIC_FUNCTIONS_INPUT_FORMAT_STRING_MAX_WIDTH_H_
